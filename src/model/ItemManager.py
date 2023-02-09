da05a71fe76ce4f6265ae4ece2cd1076889bd6ba
Shiyue Hu <75236407+RRRRRRRRonan@users.noreply.github.com>
2023-02-09 17:12:21 +0900

    Update ItemManager.py
    Add description.

--- src/model/ItemManager.py
+++ src/model/ItemManager.py
@@ -8,7 +8,7 @@ class ItemManager(metaclass=Singleton):
         self.itemList.append(item)
 
     def pick_an_item(self):
-        item = self.itemList.pop()
+        item = self.itemList.pop()  #用于移除列表中的一个元素(默认最后一个元素)，并且返回该元素的值2
         return item
 
     def remove_item(self, item):
